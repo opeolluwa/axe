719a7aac895645c177a3f9905a88e5223748e1ec
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2025-04-05 00:06:43 +0100

    update

--- nodejs/expressjs/index.js
+++ nodejs/expressjs/index.js
@@ -1,8 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import * as dotenv from 'dotenv';
-
-import { databasePool} from "./config/database.js";
+import {databasePool} from "./config/database.js";
 
 dotenv.config();
 
@@ -13,10 +12,12 @@ app.use(express.json())
 app.use(cors())
 
 app.listen(PORT, async () => {
-    await databasePool.connect().then(() => {
+    const pool = await databasePool.connect().then(() => {
         console.log("Database connected successfully ");
     }).catch(error => {
         console.log(error);
     });
+
+    await databasePool.query("CREATE TABLE IF NOT EXISTS minified_url (identifier CHAR(36), original_url VARCHAR, url_id CHAR(6))")
     console.log("ignition started on port:" + PORT);
 })
--- nodejs/expressjs/repositories/index.js
+++ nodejs/expressjs/repositories/index.js
@@ -1,19 +1,23 @@
+import { ulid } from "ulid";
+import { nanoid } from "nanoid";
+
 export class MinifiedUrlRepository {
-    databaseConnectionPool = null
+  databaseConnectionPool = null;
 
-    constructor(pool) {
-        this.databaseConnectionPool = pool
-    }
+  constructor(pool) {
+    this.databaseConnectionPool = pool;
+  }
 
-    async createMinifiedUrl() {
-    }
+  async createMinifiedUrl(originalUrl) {
+    const identifier = ulid();
+    const urlId = nanoid(6);
+    await this.databaseConnectionPool.query(
+      "INSERT INTO minified_url (identifier, original_url, url_id) VALUES ($1, $2, $3)",
+      [identifier, originalUrl, urlId]
+    );
+  }
 
-    async deleteMinifiedUrl() {
+  async deleteMinifiedUrl() {}
 
-
-    }
-
-    async getMinifiedUrl() {
-    }
-
+  async getMinifiedUrl() {}
 }
