76d27b54c47d4c41b7ecfa2dc026a269e1580433
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2025-04-14 00:29:48 +0100

    feat: update -> input and border

--- compose/app/src/main/java/com/opeolluwa/compose/MainActivity.kt
+++ compose/app/src/main/java/com/opeolluwa/compose/MainActivity.kt
@@ -1,13 +1,23 @@
 package com.opeolluwa.compose
 
+import androidx.compose.runtime.remember
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.rounded.ArrowBack
+import androidx.compose.material.icons.rounded.ArrowBack
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -18,6 +28,13 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Shapes
+import androidx.compose.material3.TextField
+import androidx.compose.ui.draw.clip
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.RectangleShape
 import com.opeolluwa.compose.ui.theme.LoginFormTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,7 +43,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             LoginFormTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(Color(0x00000000))
+                ) { innerPadding ->
                     LogInPage()
                 }
             }
@@ -37,11 +58,52 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun LogInPage(modifier: Modifier = Modifier) {
-    Column(Modifier.padding(horizontal = 10.dp, vertical = 45.dp)) {
+    var text = ""
+    Column(Modifier.padding(horizontal = 30.dp, vertical = 45.dp)) {
+        Icon(
+            Icons.AutoMirrored.Rounded.ArrowBack,
+            contentDescription = "go back to sign up page",
+            modifier = Modifier
+                .padding(bottom = 20.dp)
+                .clip(RoundedCornerShape(20.dp))
+                .background(Color(0xFF6d6d6e))
+        )
         Text(
             text = stringResource(R.string.login_page_title),
-            fontSize = 30.sp,
-            fontWeight = FontWeight.Bold
+            fontSize = 28.sp,
+            fontWeight = FontWeight.W400
         )
+
+        Text(text = "Email", Modifier.padding(top = 40.dp))
+        OutlinedTextField(
+            value = "",
+            onValueChange = { text = "" },
+//            placeholder = stringResource(R.string.email_field_placeholder),
+            modifier = Modifier
+                .height(65.dp)
+                .padding(top = 10.dp)
+                .border(color = Color(0xFFFFFFFF), shape = RectangleShape, width = 1.dp)
+                .fillMaxWidth()
+                .clip(RoundedCornerShape(12.dp))
+        )
+
+
+        Text(text = "Password", Modifier.padding(top = 40.dp))
+        OutlinedTextField(
+            value = "",
+            onValueChange = { text = "" },
+//            placeholder = stringResource(R.string.email_field_placeholder),
+            modifier = Modifier
+                .height(65.dp)
+                .padding(top = 10.dp)
+                .border(color = Color(0xFFFFFFFF), shape = RectangleShape, width = 1.dp)
+                .fillMaxWidth()
+                .clip(RoundedCornerShape(12.dp))
+        )
+        Text(text = "Forgot password?", Modifier.padding(top = 40.dp))
+
     }
 }
+
+
+
